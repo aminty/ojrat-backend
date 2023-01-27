cec6ee1b4069b52b57e8cb5ad2f8f9cc30f1c82a
amin <amintavakkoli.fi@gmail.com>
2023-01-27 23:17:19 +0330

    Initial commit

---
+++ src/main/java/com/amin/ojrat/OjratApplication.java
@@ -0,0 +1,13 @@
+package com.amin.ojrat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjratApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OjratApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amin/ojrat/OjratApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amin.ojrat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OjratApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
