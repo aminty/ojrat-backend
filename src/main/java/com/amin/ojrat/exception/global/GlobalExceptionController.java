3b56ce9e51f7371aee56d6fb8609b9312f87257c
amin <amintavakkoli.fi@gmail.com>
2023-01-29 00:30:11 +0330

    4- crate exception layer (not completed)

---
+++ src/main/java/com/amin/ojrat/exception/global/GlobalExceptionController.java
@@ -0,0 +1,8 @@
+package com.amin.ojrat.exception.global;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionController {
+}
